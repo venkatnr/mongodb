fc9c37f76cfa7864583a0f4e736f41b7360dd6cc
venkatnr <oneguyvenkat007@gmail.com>
2012-07-18 18:29:40 +0530

    first commit

---
+++ app/models/blog.rb
@@ -0,0 +1,4 @@
+class Blog
+  include Mongoid::Document
+  field :name, :type => String
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongosqlindividual::Application.config.secret_token = '6803700f254b05b8a114af7fd0a653b9bc3347a978427b0f460cef1f29e1e5caffb612af3c96f8f6bf53b3774f6b7d8289880b9f2268a6834e01ab84cf1502e4'
